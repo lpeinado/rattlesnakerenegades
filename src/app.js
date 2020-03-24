ed2fe959d82e6b9ff672f62df2bbb0664b6a28e8
Lluís Peinado <lpeinado@stelladot.com>
2020-03-24 12:37:46 +0100

    webpack stuff

---
+++ src/app.js
@@ -0,0 +1,20 @@
+import React, { Fragment } from 'react'
+import { Router, Route, Switch } from 'react-router-dom'
+import { createBrowserHistory } from 'history'
+
+
+import Home from './routes/Home'
+import About from './routes/About'
+import Sample from './routes/Sample'
+
+export const App = () => (
+  <Fragment>
+    <Router history={createBrowserHistory()}>
+      <Switch>
+        <Route exact path="/" component={Home}/>
+        <Route path="/about" component={About}/>
+        <Route path="/sample" component={Sample}/>
+      </Switch>
+    </Router>
+  </Fragment>
+)
