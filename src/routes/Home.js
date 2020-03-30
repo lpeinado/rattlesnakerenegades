a8e8dfe57e14d59c42d7e38ede9d0af8f001b075
Lluís Peinado <lpeinado@stelladot.com>
2020-03-30 16:48:48 +0200

    fist build

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -3,7 +3,7 @@ import React, { Fragment } from 'react'
 const Home = () => (
   <Fragment>
     <h1>Rattlesnake Renegades</h1>
-    <p>Welcome to the best (fiction) rock band's website</p>
+    <p>Welcome to the best (fiction) rock band's website.</p>
     <p>As you can see, we are work in progress, soon you'l be able to read the whole comicbook here! :-D</p>
   </Fragment>
 )
