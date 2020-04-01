ff860c8fe1b6c0db1ddc7460536c5275da78d511
Lluís Peinado <lpeinado@stelladot.com>
2020-04-01 17:51:44 +0200

    Installing Bulma on site

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,10 +1,17 @@
+/* eslint-disable react/no-unescaped-entities */
 import React, { Fragment } from 'react'
 
 const Home = () => (
   <Fragment>
-    <h1>Rattlesnake Renegades</h1>
-    <p>Welcome to the best (fiction) rock band's website.</p>
-    <p>As you can see, we are work in progress, soon you'l be able to read the whole comicbook here! :-D</p>
+    <section className="section">
+      <div className="container">
+        <h1 className="title">
+        Rattlesnake Renegades
+        </h1>
+        <p className="subTitle">Welcome to the best (fiction) rock band's website.</p>
+        <p>As you can see, we are work in progress, soon you'l be able to read the whole comicbook here! :-D</p>
+      </div>
+    </section>
   </Fragment>
 )
 
