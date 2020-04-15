7bf074a20739c64cadb2a35b31dc2595036c73a0
Lluís Peinado <lpeinado@stelladot.com>
2020-04-15 10:52:31 +0200

    Cover and transitions

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,16 +1,22 @@
 /* eslint-disable react/no-unescaped-entities */
 import React, { Fragment } from 'react'
+import cover from '../assets/images/cover.jpg'
 
 const Home = () => (
   <Fragment>
     <section className="section">
       <div className="container">
-        <h1 className="title">
-        Rattlesnake Renegades
-        </h1>
-        <p className="subTitle">Welcome to the best (fiction) rock band's website.</p>
-        <p>As you can see, we are work in progress, soon you'l be able to read the whole comicbook here! :-D</p>
       </div>
+      <figure className="center">
+        <img src={cover} className=""/>
+        <figcaption>
+          <h1 className="title">
+            Rattlesnake Renegades
+          </h1>
+          <p className="subTitle">Welcome to the best (fiction) rock band's website.</p>
+          <p>As you can see, we are work in progress, soon you'l be able to read the whole comicbook here!</p>
+        </figcaption>
+      </figure>
     </section>
   </Fragment>
 )
