1ccf181a78a3f94494aa188acc298e942e6e68b1
Lluís Peinado <lpeinado@stelladot.com>
2020-04-16 12:46:23 +0200

    Style adjustments

--- src/app.js
+++ src/app.js
@@ -2,7 +2,6 @@ import React, { Fragment } from 'react'
 import { Router, Route, Switch } from 'react-router-dom'
 import { createBrowserHistory } from 'history'
 
-
 import Home from './routes/Home'
 import About from './routes/About'
 import Sample from './routes/Sample'
--- src/routes/Sample.js
+++ src/routes/Sample.js
@@ -8,7 +8,7 @@ const Sample = () => {
       Count: {counter}
       <button onClick={() => setCounter(counter + 1)}>Add</button>
       <article>
-        <h1>Fancy title here</h1>
+        <h1>Rattlesnake Renegades Band</h1>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
       </article>
     </div>
